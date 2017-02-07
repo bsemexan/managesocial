e1c90eff57ed3b6167dfb7894a41d48b1d4445ab
berry semexan <bsemexan@gmail.com>
2017-02-06 21:40:01 -0500

    update

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -210,14 +210,13 @@ Type::build('timestamp')
  * Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
  * advanced ways of loading plugins
  */
- Plugin::loadAll(); // Loads all plugins at once
+ 
  Plugin::load('Migrations'); //Loads a single plugin named Migrations
-
+ Plugin::load('DebugKit', ['autoload' => true]);
+ Plugin::load('DebugKit', ['bootstrap' => true]);
+ Plugin::loadAll(); // Loads all plugins at once
 
 /*
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
-}
